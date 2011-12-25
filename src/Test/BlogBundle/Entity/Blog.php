79eddf2b00df24413c5b3fd56d7cb739261a2e9f
kovalev <all@yurakovalev.ru>
2011-12-25 23:35:56 +0400

    setters, getters

--- src/Test/BlogBundle/Entity/Blog.php
+++ src/Test/BlogBundle/Entity/Blog.php
@@ -29,4 +29,34 @@ class Blog
     private $title;
 
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 }
--- src/Test/BlogBundle/Entity/Post.php
+++ src/Test/BlogBundle/Entity/Post.php
@@ -46,4 +46,74 @@ class Post
     private $blog;
 
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set body
+     *
+     * @param text $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * Get body
+     *
+     * @return text 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set blog
+     *
+     * @param Test\BlogBundle\Entity\Blog $blog
+     */
+    public function setBlog(\Test\BlogBundle\Entity\Blog $blog)
+    {
+        $this->blog = $blog;
+    }
+
+    /**
+     * Get blog
+     *
+     * @return Test\BlogBundle\Entity\Blog 
+     */
+    public function getBlog()
+    {
+        return $this->blog;
+    }
 }
